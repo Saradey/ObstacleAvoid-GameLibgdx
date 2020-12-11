9319360f436fa6e42a51adabb5552de0f8746d3f
Evgeny Goncharov <Evgenii.Goncharov@excelian.com>
2020-12-11 17:59:20 +0300

    end 72. Refactor Time - create GameObjectBase class

---
+++ core/src/com/obstacle/avoid/entity/GameObjectBase.java
@@ -0,0 +1,50 @@
+package com.obstacle.avoid.entity;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+
+/**
+ * Created by goran on 25/08/2016.
+ */
+public abstract class GameObjectBase {
+
+    private float x;
+    private float y;
+    private Circle bounds;
+
+    public GameObjectBase(float boundsRadius) {
+        bounds = new Circle(x, y, boundsRadius);
+    }
+
+    public void drawDebug(ShapeRenderer renderer) {
+        renderer.circle(bounds.x, bounds.y, bounds.radius, 30);
+    }
+
+    public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+        updateBounds();
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+        updateBounds();
+    }
+
+    public void setY(float y) {
+        this.y = y;
+        updateBounds();
+    }
+
+    public void updateBounds() {
+        bounds.setPosition(x, y);
+    }
+}
--- core/src/com/obstacle/avoid/entity/Obstacle.java
+++ core/src/com/obstacle/avoid/entity/Obstacle.java
@@ -8,46 +8,19 @@ import com.badlogic.gdx.math.Circle;
 /**
  * Created by goran on 23/08/2016.
  */
-public class Obstacle {
+public class Obstacle extends GameObjectBase {
 
     private static final float BOUNDS_RADIUS = 0.3f; // world units
     private static final float SIZE = 2 * BOUNDS_RADIUS;
 
-    private float x;
-    private float y;
-
     private float ySpeed = 0.1f;
 
-    private Circle bounds;
-
     public Obstacle() {
-        bounds = new Circle(x, y, BOUNDS_RADIUS);
-    }
-
-    public void drawDebug(ShapeRenderer renderer) {
-        renderer.circle(bounds.x, bounds.y, bounds.radius, 30);
-    }
-
-    public void setPosition(float x, float y) {
-        this.x = x;
-        this.y = y;
-        updateBounds();
+        super(BOUNDS_RADIUS);
     }
 
     public void update() {
-        setPosition(x, y - ySpeed);
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    private void updateBounds() {
-        bounds.setPosition(x, y);
+        setY(getY() - ySpeed);
     }
 
     public float getWidth() {
