83c659569d2f62ab77db01e20ce43a2673d473cc
Evgeny Goncharov <Evgenii.Goncharov@excelian.com>
2020-12-13 19:57:10 +0300

    end 82. GameController Class

--- core/src/com/obstacle/avoid/ObstacleAvoidGame.java
+++ core/src/com/obstacle/avoid/ObstacleAvoidGame.java
@@ -3,14 +3,14 @@ package com.obstacle.avoid;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.obstacle.avoid.screen.GameScreen;
+import com.obstacle.avoid.screen.GameScreenOld;
 
 public class ObstacleAvoidGame extends Game {
 
     @Override
     public void create() {
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
-        setScreen(new GameScreen());
+        setScreen(new GameScreenOld());
     }
 
 }
---
+++ core/src/com/obstacle/avoid/screen/GameScreenOld.java
@@ -0,0 +1,250 @@
+package com.obstacle.avoid.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Logger;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.obstacle.avoid.assets.AssetPaths;
+import com.obstacle.avoid.config.DifficultyLevel;
+import com.obstacle.avoid.config.GameConfig;
+import com.obstacle.avoid.entity.Obstacle;
+import com.obstacle.avoid.entity.Player;
+import com.obstacle.avoid.utils.ViewportUtils;
+import com.obstacle.avoid.utils.debug.DebugCameraController;
+
+@Deprecated
+public class GameScreenOld implements Screen {
+
+    private static final Logger log = new Logger(GameScreenOld.class.getName(), Logger.DEBUG);
+
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private ShapeRenderer renderer;
+    private Player player;
+    private DebugCameraController debugCameraController;
+    private final Array<Obstacle> obstacles = new Array<Obstacle>();
+    private float obstacleTimer;
+
+    //Ui камера
+    private OrthographicCamera hudCamera;
+    private Viewport hudViewport;
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private final GlyphLayout layout = new GlyphLayout();
+
+    private int lives = GameConfig.LIVES_START;
+    private float scoreTimer;
+    private int score;
+    private int displayScore = 0;
+    private final DifficultyLevel difficultyLevel = DifficultyLevel.MEDIUM;
+
+
+    //используем его для инициализации нашей игры и загружайте ресурсы
+    @Override
+    public void show() {
+        hudCamera = new OrthographicCamera();
+        hudViewport = new FitViewport(GameConfig.HUD_WIDTH, GameConfig.HUD_HEIGHT, hudCamera);
+        batch = new SpriteBatch();
+        font = new BitmapFont(Gdx.files.internal(AssetPaths.UI_FONT));
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT, camera);
+        renderer = new ShapeRenderer();
+
+        // create player
+        player = new Player();
+        // calculate position
+        float startPlayerX = GameConfig.WORLD_WIDTH / 2;
+        float startPlayerY = 1;
+        // position player
+        player.setPosition(startPlayerX, startPlayerY);
+
+        // create debug camera controller
+        debugCameraController = new DebugCameraController();
+        debugCameraController.setStartPosition(GameConfig.WORLD_CENTER_X, GameConfig.WORLD_CENTER_Y);
+    }
+
+    @Override
+    public void render(float delta) {
+        //camera control
+        debugCameraController.handleDebugInput(delta);
+        debugCameraController.applyTo(camera);
+
+        // update world
+        update(delta);
+
+        //clear screen
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // render ui/hud
+        renderUi();
+
+        //render debug
+        renderDebug();
+    }
+
+    private void renderUi() {
+        batch.setProjectionMatrix(hudCamera.combined);
+        batch.begin();
+
+        String livesText = "LIVES: " + lives;
+        layout.setText(font, livesText);
+        font.draw(batch, livesText,
+                20,
+                GameConfig.HUD_HEIGHT - layout.height);
+
+        String scoreText = "SCORE: " + displayScore;
+        layout.setText(font, scoreText);
+        font.draw(batch, scoreText,
+                GameConfig.HUD_WIDTH - layout.width - 20,
+                GameConfig.HUD_HEIGHT - layout.height
+        );
+
+        batch.end();
+    }
+
+    @Deprecated
+    private void update(float delta) {
+        if(gameIsOver()) {
+            log.debug("Game Over");
+            return;
+        }
+        updatePlayer();
+        updateObstacles(delta);
+        updateScore(delta);
+        updateDisplayScore(delta);
+        if (isPlayerCollidingWithObstacle()) {
+            log.debug("Collision detected.");
+            lives--;
+        }
+    }
+
+    private boolean gameIsOver() {
+        return lives <= 0;
+    }
+
+    private boolean isPlayerCollidingWithObstacle() {
+        for (Obstacle obstacle : obstacles) {
+            if (obstacle.isNotHit() && obstacle.isPlayerColliding(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void updateObstacles(float delta) {
+        for (Obstacle obstacle : obstacles) {
+            obstacle.update();
+        }
+
+        createNewObstacle(delta);
+    }
+
+    private void createNewObstacle(float delta) {
+        obstacleTimer += delta;
+
+        if (obstacleTimer >= GameConfig.OBSTACLE_SPAWN_TIME) {
+            float min = 0f;
+            float max = GameConfig.WORLD_WIDTH;
+            float obstacleX = MathUtils.random(min, max);
+            float obstacleY = GameConfig.WORLD_HEIGHT;
+
+            Obstacle obstacle = new Obstacle();
+            obstacle.setySpeed(difficultyLevel.getObstacleSpeed());
+            obstacle.setPosition(obstacleX, obstacleY);
+
+            obstacles.add(obstacle);
+            obstacleTimer = 0f;
+        }
+    }
+
+    private void blockPlayerFromLeavingTheWorld() {
+        float playerX = MathUtils.clamp(
+                player.getX(), // value
+                player.getWidth() / 2f, // min
+                GameConfig.WORLD_WIDTH - player.getWidth() / 2f // max
+        );
+        player.setPosition(playerX, player.getY());
+    }
+
+    private void updatePlayer() {
+        player.update();
+        blockPlayerFromLeavingTheWorld();
+    }
+
+    private void renderDebug() {
+        renderer.setProjectionMatrix(camera.combined);
+        renderer.begin(ShapeRenderer.ShapeType.Line);
+        drawDebug();
+        renderer.end();
+
+        ViewportUtils.drawGrid(viewport, renderer);
+    }
+
+    private void drawDebug() {
+        player.drawDebug(renderer);
+
+        for (Obstacle obstacle : obstacles) {
+            obstacle.drawDebug(renderer);
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+        hudViewport.update(width, height, true);
+        ViewportUtils.debugPixelPerUnit(viewport);
+    }
+
+    private void updateScore(float delta) {
+        scoreTimer += delta;
+
+        if (scoreTimer >= GameConfig.SCORE_MAX_TIME) {
+            score += MathUtils.random(1, 5);
+            scoreTimer = 0.0f;
+        }
+    }
+
+    private void updateDisplayScore(float delta) {
+        if(displayScore < score) {
+            displayScore = Math.min(
+                    score,
+                    displayScore + (int)(60 * delta)
+            );
+        }
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    //вызывается когда мы прячим скрин к примеру когда мы переходим из одного экрана к другому
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    //не вызывается автоматически
+    @Override
+    public void dispose() {
+        renderer.dispose();
+        batch.dispose();
+        font.dispose();
+    }
+}
