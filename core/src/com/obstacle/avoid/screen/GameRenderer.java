aff90a0acfc9bea9f343ef6e4519d9dd07b31303
Evgeny Goncharov <Evgenii.Goncharov@excelian.com>
2020-12-13 20:04:14 +0300

    end 83. Game Renderer Class

---
+++ core/src/com/obstacle/avoid/screen/GameRenderer.java
@@ -0,0 +1,131 @@
+package com.obstacle.avoid.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.obstacle.avoid.assets.AssetPaths;
+import com.obstacle.avoid.config.GameConfig;
+import com.obstacle.avoid.utils.ViewportUtils;
+import com.obstacle.avoid.utils.debug.DebugCameraController;
+
+/**
+ * Created by goran on 27/08/2016.
+ */
+public class GameRenderer implements Disposable {
+
+    // == attributes ==
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private ShapeRenderer renderer;
+
+    private OrthographicCamera hudCamera;
+    private Viewport hudViewport;
+
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private final GlyphLayout layout = new GlyphLayout();
+    private DebugCameraController debugCameraController;
+
+    // == constructors ==
+    public GameRenderer() {
+        init();
+    }
+
+    // == init ==
+    private void init() {
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT, camera);
+        renderer = new ShapeRenderer();
+
+        hudCamera = new OrthographicCamera();
+        hudViewport = new FitViewport(GameConfig.HUD_WIDTH, GameConfig.HUD_HEIGHT, hudCamera);
+        batch = new SpriteBatch();
+        font = new BitmapFont(Gdx.files.internal(AssetPaths.UI_FONT));
+
+        // create debug camera controller
+        debugCameraController = new DebugCameraController();
+        debugCameraController.setStartPosition(GameConfig.WORLD_CENTER_X, GameConfig.WORLD_CENTER_Y);
+    }
+
+    // == public methods ==
+    public void render(float delta) {
+        // not wrapping inside alive cuz we want to be able to control camera even when there is game over
+        debugCameraController.handleDebugInput(delta);
+        debugCameraController.applyTo(camera);
+
+        // clear screen
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // render ui/hud
+        renderUi();
+
+        // render debug graphics
+        renderDebug();
+    }
+
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+        hudViewport.update(width, height, true);
+        ViewportUtils.debugPixelPerUnit(viewport);
+    }
+
+    @Override
+    public void dispose() {
+        renderer.dispose();
+        batch.dispose();
+        font.dispose();
+    }
+
+    // == private methods ==
+
+    private void renderUi() {
+        batch.setProjectionMatrix(hudCamera.combined);
+        batch.begin();
+
+        String livesText = "LIVES: " + lives;
+        layout.setText(font, livesText);
+
+        font.draw(batch, livesText,
+                20,
+                GameConfig.HUD_HEIGHT - layout.height
+        );
+
+        String scoreText = "SCORE: " + displayScore;
+        layout.setText(font, scoreText);
+
+        font.draw(batch, scoreText,
+                GameConfig.HUD_WIDTH - layout.width - 20,
+                GameConfig.HUD_HEIGHT - layout.height
+        );
+
+        batch.end();
+    }
+
+    private void renderDebug() {
+
+        renderer.setProjectionMatrix(camera.combined);
+        renderer.begin(ShapeRenderer.ShapeType.Line);
+
+        drawDebug();
+
+        renderer.end();
+
+        ViewportUtils.drawGrid(viewport, renderer);
+    }
+
+    private void drawDebug() {
+        player.drawDebug(renderer);
+
+        for (Obstacle obstacle : obstacles) {
+            obstacle.drawDebug(renderer);
+        }
+    }
+}
